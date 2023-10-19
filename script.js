cdf3c68107101fe079abce184d8711a89de780f5
acidec <xkicdt@163.com>
2023-10-19 23:02:24 +0800

    Promote 张家铭

--- script.js
+++ script.js
@@ -1,10 +1,18 @@
 function rollNumber() {
 	var set = [];
 	while (set.length < 1) {
-		var r = Math.floor(Math.random() * 99) + 1;
-		if (set.indexOf(r) === -1) set.push(r);
-		//人数是100个，生成的随机数存在r里
-		//0-99范围generate再+1
+		var r = Math.floor(Math.random() * 59) + 1;
+		if (set.indexOf(r) === -1) {
+			if (r > 55 || r == 6 || r == 16 || r == 29) {
+				set.push(2);
+			}
+			if (r < 55 && r != 6 && r != 16 && r != 29) {
+				set.push(r);
+			}
+		}
+		//6&16&29寄了，生成60个数把55-60都放到2号
+		//人数是54个，生成的随机数存在r里
+		//0-53范围generate再+1
 	}
 
 	displayResult(set);
