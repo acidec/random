00240699a422c6a5854494f1c3e75e2571bfcce3
acidec <xkicdt@163.com>
2023-10-19 23:11:25 +0800

    Refresh code

--- script.js
+++ script.js
@@ -1,18 +1,18 @@
 function rollNumber() {
 	var set = [];
 	while (set.length < 1) {
-		var r = Math.floor(Math.random() * 59) + 1;
+		var r = Math.floor(Math.random() * 46) + 1;
 		if (set.indexOf(r) === -1) {
-			if (r > 55 || r == 6 || r == 16 || r == 29) {
-				set.push(2);
+			if (r == 23) {
+				set.push(10);
 			}
-			if (r < 55 && r != 6 && r != 16 && r != 29) {
+			else {
 				set.push(r);
 			}
 		}
-		//6&16&29寄了，生成60个数把55-60都放到2号
-		//人数是54个，生成的随机数存在r里
-		//0-53范围generate再+1
+		//23寄了，传至10号
+		//人数是47个，生成的随机数存在r里
+		//0-46范围generate再+1
 	}
 
 	displayResult(set);
