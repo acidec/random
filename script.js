3665feeaae93cedc5628bd73d8abb94e2d3f6c84
acidec <xkicdt@163.com>
2023-10-22 19:45:44 +0800

    v3.1.2 2117 Edition Commit

--- script.js
+++ script.js
@@ -1,25 +1,41 @@
 function rollNumber() {
 	var set = [];
 	while (set.length < 1) {
-		var r = Math.floor(Math.random() * 46) + 1;
-		if (set.indexOf(r) === -1) {
-			if (r == 23) {
-				set.push(10);
+		if (e1 == 0) set.push("所有人都被点过了，您需要重置");
+		if (e1 != 0) {
+			while (num[r] == 0) {
+				r = Math.floor(Math.random() * 46) + 1;
 			}
-			else {
-				set.push(r);
+			num[r] = num[r] - 1;
+			//num数组用来判断Math.random()生成的数是否被点过了
+			//一开始，所有值都是1，被点过了就把这个标记改为0
+			//如果抽到了值为0的项，就重新生成一次
+			//如果值为1，进入输出
+			e1 = e1 - 1;
+			//每次抽的时候，e1都会减1
+			//到0时，停止响应并输出重置要求
+
+			document.getElementById("e1/e2").innerHTML = e1 + "/" + e2;
+
+			if (set.indexOf(r) === -1) {
+				if (r == 23) set.push(47);
+
+				else set.push(r);
 			}
+			//人数是100个，生成的随机数存在r里
+			//0-99范围generate再+1
 		}
-		//23寄了，传至10号
-		//人数是47个，生成的随机数存在r里
-		//0-46范围generate再+1
+
+		displayResult(set);
 	}
-
-	displayResult(set);
 }
-
 function displayResult(set) {
 	set.join(", ");
 
 	document.getElementById('result').innerHTML = "<center><h1 class='text-primary'>" + set + "</h1></center>";
+}
+function reset() {
+	e1 = 46;
+	for (let i = 1; i <= 47; i++) num[i] = 1;
+	document.getElementById("e1/e2").innerHTML = e1 + "/" + e2;
 }
